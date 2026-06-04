d4b644051b42ac218e6447bfc77d0efe8fb43b10
Joaquin Mariscal <ellucho042@gmail.com>
2026-06-04 17:12:33 -0400

    subida solo backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,16 +7,25 @@ import { ProductsModule } from './products/products.module';
 import { VentasModule } from './sales/sales.module';
 import { UsuariosModule } from './users/users.module';
 import { DetallesVentasModule } from './detalle_venta/detalle_venta.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { config } from 'process';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      database: 'ryztor',
-      username: 'postgres',
-      password: '123456',
-      entities: [__dirname + '/**/*.entity{.ts,.js}']
+    ConfigModule.forRoot({isGlobal:true}),
+    TypeOrmModule.forRootAsync({
+      imports:[ConfigModule],
+      useFactory: async (ConfigService: ConfigService) =>({
+        type: 'postgres',
+        host: ConfigService.get('DB_HOST'),
+        port: ConfigService.get<number>('DB_PORT'),
+        username: ConfigService.get('DB_USERNAME'),
+        password: ConfigService.get('DB_PASSWORD'),
+        database: ConfigService.get('DB_DATABASE'),
+        autoLoadEntities:true,
+        synchronize:true
+      }),
+      inject: [ConfigService]
     })
     , AuthModule, DeliveriesModule, LogsModule, ProductsModule, VentasModule, UsuariosModule, DetallesVentasModule],
   controllers: [],
